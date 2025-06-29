8595271d8b8ca0ed9e03c9261cf742b596cb8f96
malkoG <rijgndqw012@gmail.com>
2025-06-29 21:14:31 +0900

    Integrate with Fedify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Inject, MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { FEDIFY_FEDERATION, FedifyModule, integrateFederation } from "./fedify";
+import { DatabaseModule } from './database/database.module';
+import { FederationModule } from './modules/federation/federation.module';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		DatabaseModule,
+		FedifyModule.forRoot({
+			// Federation options here
+		}),
+		FederationModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	constructor(
+		@Inject(FEDIFY_FEDERATION) private federation: any,
+		private dataSource: DataSource,
+	) {}
+
+	configure(consumer: MiddlewareConsumer) {
+		const fedifyMiddleware = integrateFederation(this.federation, async (req, res) => {
+			// Create rich context with database access and request info
+			return {
+				request: req,
+				response: res,
+				dataSource: this.dataSource,
+				url: new URL(req.url, `${req.protocol}://${req.get('host')}`),
+			};
+		});
+		
+		// Apply middleware to all routes
+		consumer
+			.apply(fedifyMiddleware)
+			.forRoutes({ path: '*', method: RequestMethod.ALL });
+	}
+}
---
+++ src/fedify/index.ts
@@ -0,0 +1,7 @@
+export * from './fedify.module';
+export * from './fedify.interfaces';
+export * from './fedify.constants';
+export * from './fedify.middleware';
+export * from './integrate-federation';
+export * from './fedify.context';
+export * from './fedify.handler-setup';
